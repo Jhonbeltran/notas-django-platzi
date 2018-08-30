3d94e0913a901b8005aba4272ce5ad6ddb06ff32
Jhonbeltran <jbeltranleon@gmail.com>
2018-08-30 13:45:09 -0500

    corrijo birth_date

--- add_model_elements_using_shell_script.py
+++ add_model_elements_using_shell_script.py
@@ -19,7 +19,7 @@ users = [
         'first_name': 'Yésica',
         'last_name': 'Cortés',
         'password': 'qwerty',
-        'birthdate': date(1990, 12,19)
+        'birth_date': date(1990, 12,19)
     },
     {
         'email': 'arturo@platzi.com',
@@ -27,14 +27,14 @@ users = [
         'last_name': 'Martínez',
         'password': 'msicomputer',
         'is_admin': True,
-        'birthdate': date(1981, 11, 6),
+        'birth_date': date(1981, 11, 6),
         'bio': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     }
 ]
 
 from posts.models import User
 
-for user inusers:
+for user in users:
     obj = User(**user)
     obj.save()
     print(obj.pk, ':', obj.email)
